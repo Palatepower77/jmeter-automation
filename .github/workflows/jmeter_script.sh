e6a7aa4df44396d66ab5c80d4886ff1fc6113a2e
Gauri-22 <97720903+Gauri-22@users.noreply.github.com>
2023-07-04 16:55:30 +0530

    Update jmeter_script.sh

--- .github/workflows/jmeter_script.sh
+++ .github/workflows/jmeter_script.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
-echo 'Download JMeter'
-wget -O jmeter.tgz https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz
-tar -xzf jmeter.tgz
+run_jmeter() {
+  echo 'Download JMeter'
+  wget -O jmeter.tgz https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz
+  tar -xzf jmeter.tgz
 
-echo 'Run JMeter Test'
-./apache-jmeter-5.5/bin/jmeter -JTOKEN="${TOKEN}" -n -t Palate_App.jmx -l result.jtl > jmeter_output.log
-echo "$(cat jmeter_output.log)"
+  echo 'Run JMeter Test'
+  ./apache-jmeter-5.5/bin/jmeter -JTOKEN="${TOKEN}" -n -t Palate_App.jmx -l result.jtl > jmeter_output.log
+  echo "$(cat jmeter_output.log)"
 
-echo 'Convert JTL to CSV'
-sed -e 's/<[^>]*>//g' result.jtl > result.csv
+  echo 'Convert JTL to CSV'
+  sed -e 's/<[^>]*>//g' result.jtl > result.csv
+}
 
-echo 'Upload Result Files as Artifacts'
-mkdir artifacts
-mv result.csv artifacts/result.csv
-mv result.jtl artifacts/result.jtl
+check_result_errors() {
+  echo 'Check Result Errors'
+  jmeter_output=$(cat jmeter_output.log)
+  summary=$(echo "$jmeter_output" | grep -o "summary +.*")
+  while read -r line; do
+    error_count=$(echo "$line" | grep -o "Err: *[0-9]*" | awk -F' ' '{print $NF}')
+    if [ "$error_count" -gt 0 ]; then
+      echo "There were errors in the JMeter result"
+      exit 1
+    fi
+  done <<< "$summary"
+  echo "No errors found in the JMeter result"
+}
 
-# Save the artifact path to an environment file
-echo "ARTIFACT_PATH=artifacts" >> $GITHUB_ENV
-
-echo 'Check Result Errors'
-jmeter_output=$(cat jmeter_output.log)
-summary=$(echo "$jmeter_output" | grep -o "summary +.*")
-while read -r line; do
-  error_count=$(echo "$line" | grep -o "Err: *[0-9]*" | awk -F' ' '{print $NF}')
-  if [ "$error_count" -gt 0 ]; then
-    echo "There were errors in the JMeter result"
-    exit 1
-  fi
-done <<< "$summary"
-echo "No errors found in the JMeter result"
+# Check the command-line argument and call the appropriate function
+if [ "$1" == "run_jmeter" ]; then
+  run_jmeter
+elif [ "$1" == "check_result_errors" ]; then
+  check_result_errors
+else
+  echo "Invalid function name"
+  exit 1
+fi
